d51aa3e00b2b05e5a9fa0e3dcc944c72f568c775
GitHub Action <action@github.com>
2020-12-21 22:22:50 +0000

    Updating documentation

--- scripts/node-logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x -e
-cd /quickstart-linux-utilities
-source quickstart-cfn-tools.source
-qs_retry_command 50 ls /opt/corda/logs/*log
-qs_cloudwatch_tracklog $(/bin/ls /opt/corda/logs/*log)
